f4d059d4074787f1425d9ba60c3d804ce3c3361c
Thien <thienkphan@gmail.com>
2020-02-17 21:20:49 +0700

    init

---
+++ store.js
@@ -0,0 +1,67 @@
+class Store {
+	constructor(state, reducer, namespace) {
+		this.state = state;
+		this.reducer = reducer;
+		this.reducer._callbacks = [];
+		this.dispatch = this.dispatch.bind(this);
+	}
+
+	subscribe(callback) {
+		// Subscribe middlewares
+		const idxCb = this.reducer._callbacks.push(callback);
+		return () =>	this.reducer._callbacks.splice(idxCb - 1, 1);
+	}
+
+	dispatch(actionType) {
+		// Fire all middlewares
+		 if (this.reducer._callbacks && this.reducer._callbacks.length > 0) {
+			this.reducer._callbacks.map(_callback => _callback(actionType));
+		}
+		this.state = this.reducer(actionType, this.state);
+	}
+}
+const initialState =  {
+	count: 0,
+	todo: {
+		items: []
+	}
+}
+
+function storeReducer(action, state) {
+	const type = action.type;
+		switch(type) {
+				case 'ADD_TODOS': {
+					return Object.assign({}, {...state, todo: [...state.todo.items, action.payload]});
+				}
+				case 'INCREMENT': {
+					return Object.assign({}, state, {count: state.count + action.payload});
+				}
+
+				default: {
+					return state;
+				}
+	}
+}
+
+const actions = {
+	increment(payload) {
+		return {
+			type: 'INCREMENT',
+			payload
+		}
+	},
+	addItems(payload) {
+		return {
+			type: 'ADD_TODOS',
+			payload
+		}
+	}
+}
+
+const store = new Store(initialState, storeReducer);
+const unsub = store.subscribe((actionType) => {
+	console.log('Mutate', {type: actionType.type, payload: actionType.payload})
+});
+
+store.dispatch(actions.addItems({text: 'aaaa'}));
+console.log(store.state);
