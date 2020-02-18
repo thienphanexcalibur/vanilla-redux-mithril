729f9ede4979c3289e0e12243fe2cd59df458484
Phan Khanh Thien <thien061097@gmail.com>
2020-02-18 14:32:25 +0700

    Update store.js

--- store.js
+++ store.js
@@ -1,25 +1,41 @@
 class Store {
-    constructor(state, reducer, namespace) {
-        this.state = state;
-        this.reducer = reducer;
-        this.reducer._callbacks = [];
-        this.dispatch = this.dispatch.bind(this);
-    }
+	constructor(state, reducer, namespace) {
+		this.state = state;
+		this.reducer = reducer;
+		this.reducer._callbacks = [];
+		this.dispatch = this.dispatch.bind(this);
+		this.namespace = namespace;
+		this.connect = this.connect.bind(this, this.state);
+		this.getState = this.getState.bind(this);
+	}
+	subscribe(callback) {
+		// Subscribe middlewares
+		const idxCb = this.reducer._callbacks.push(callback);
+		return () =>	this.reducer._callbacks.splice(idxCb - 1, 1);
+	}
+	getState() {
+		return this.state
+	}
 
-    subscribe(callback) {
-        // Subscribe middlewares
-        const idxCb = this.reducer._callbacks.push(callback);
-        return () => this.reducer._callbacks.splice(idxCb - 1, 1);
-    }
+	connect(state, mappedFunc) {
+		let isEmpty = false;
+		const context = this;
+		return (component => ({
+				view(vnode) {
+					return m(component, mappedFunc(mappedFunc(context.getState())));
+				}
+		}));
+	}
 
-    dispatch(actionType) {
-        // Fire all middlewares
-        if (this.reducer._callbacks && this.reducer._callbacks.length > 0) {
-            this.reducer._callbacks.map(_callback => _callback(actionType));
-        }
-        this.state = this.reducer(actionType, this.state);
-    }
+	dispatch(actionType) {
+		// Fire all middlewares
+		 if (this.reducer._callbacks && this.reducer._callbacks.length > 0) {
+			this.reducer._callbacks.map(_callback => _callback(actionType));
+		}
+		this.state = this.reducer(actionType, this.state);
+	}
 }
+
 const initialState = {
     count: 0,
     todo: {
