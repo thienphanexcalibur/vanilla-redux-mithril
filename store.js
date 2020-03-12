081cc5dab92119cf48cbf110a2e823996748e341
Phan Khanh Thien <thien061097@gmail.com>
2020-03-12 22:15:58 +0700

    Prettify

--- store.js
+++ store.js
@@ -1,96 +1,96 @@
 import m from 'mithril';
 
 class Store {
-  constructor(state, reducer, namespace) {
-	this.state = state;
-	this.reducer = reducer;
-	this.reducer._callbacks = [];
-	this.dispatch = this.dispatch.bind(this);
-	this.namespace = namespace;
-	this.connect = this.connect.bind(this, this.state);
-	this.getState = this.getState.bind(this);
-  }
-  subscribe(callback) {
-	// Subscribe middlewares
-	const idxCb = this.reducer._callbacks.push(callback);
-	return () =>	this.reducer._callbacks.splice(idxCb - 1, 1);
-  }
-  getState() {
-	return this.state
-  }
+    constructor(state, reducer, namespace) {
+        this.state = state;
+        this.reducer = reducer;
+        this.reducer._callbacks = [];
+        this.dispatch = this.dispatch.bind(this);
+        this.namespace = namespace;
+        this.connect = this.connect.bind(this, this.state);
+        this.getState = this.getState.bind(this);
+    }
+    subscribe(callback) {
+        // Subscribe middlewares
+        const idxCb = this.reducer._callbacks.push(callback);
+        return () => this.reducer._callbacks.splice(idxCb - 1, 1);
+    }
+    getState() {
+        return this.state
+    }
 
-  connect(state, mappedFunc) {
-	let isEmpty = false;
-	const context = this;
-	return (component => ({
-			view(vnode) {
-				return m(component, mappedFunc(mappedFunc(context.getState())));
-			}
-	}));
-  }
+    connect(state, mappedFunc) {
+        let isEmpty = false;
+        const context = this;
+        return (component => ({
+            view(vnode) {
+                return m(component, mappedFunc(mappedFunc(context.getState())));
+            }
+        }));
+    }
 
-  dispatch(actionType) {
-	// Fire all middlewares
-	 if (this.reducer._callbacks && this.reducer._callbacks.length > 0) {
-		this.reducer._callbacks.map(_callback => _callback(actionType));
-	}
-	this.state = this.reducer(actionType, this.state);
-  }
+    dispatch(actionType) {
+        // Fire all middlewares
+        if (this.reducer._callbacks && this.reducer._callbacks.length > 0) {
+            this.reducer._callbacks.map(_callback => _callback(actionType));
+        }
+        this.state = this.reducer(actionType, this.state);
+    }
 }
 
 const initialState = {
-  count: 0,
-  todo: {
-	  items: []
-  }
+    count: 0,
+    todo: {
+        items: []
+    }
 }
 
 function storeReducer(action, state) {
-  const type = action.type;
-  switch (type) {
-	case 'ADD_TODOS': {
-		return Object.assign({}, state, {
-			todo: [...state.todo.items, action.payload] }
-		)
-	}
+    const type = action.type;
+    switch (type) {
+        case 'ADD_TODOS': {
+            return Object.assign({}, state, {
+                todo: [...state.todo.items, action.payload]
+            })
+        }
 
-	case 'INCREMENT': {
-		return Object.assign({}, state, {
-			count: state.count + action.payload
-		});
-	}
+        case 'INCREMENT': {
+            return Object.assign({}, state, {
+                count: state.count + action.payload
+            });
+        }
 
-	default: {
-		return state;
-	}
-  }
+        default: {
+            return state;
+        }
+    }
 }
 
 const actions = {
-  increment(payload) {
-	return {
-	  type: 'INCREMENT',
-	  payload
-	}
-  },
-  addItems(payload) {
-	return {
-	  type: 'ADD_TODOS',
-	  payload
-	}
-  }
+    increment(payload) {
+        return {
+            type: 'INCREMENT',
+            payload
+        }
+    },
+    addItems(payload) {
+        return {
+            type: 'ADD_TODOS',
+            payload
+        }
+    }
 }
 
 const store = new Store(initialState, storeReducer);
 const unsub = store.subscribe((actionType) => {
-  console.log('Mutate', {
-	type: actionType.type,
-	payload: actionType.payload
-  })
+    console.log('Mutate', {
+        type: actionType.type,
+        payload: actionType.payload
+    })
 });
 
 store.dispatch(actions.addItems({
-  text: 'aaaa'
+    text: 'aaaa'
 }));
 
 console.log(store.state);
